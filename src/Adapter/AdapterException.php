c80f0f159d5faa2abca956d058667363b2363190
boite <boite@users.noreply.github.com>
2017-05-14 15:02:32 +0100

    Add Bergen adapter.

---
+++ src/Adapter/AdapterException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ObjectStorage\Adapter;
+
+use Exception;
+
+class AdapterException extends Exception
+{
+}
---
+++ src/Adapter/BergenAdapter.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace ObjectStorage\Adapter;
+
+use Bergen\Client\ClientBuilder;
+use Bergen\Client\ClientOptions;
+use Bergen\Client\Error\UnexpectedResponseError;
+use Bergen\Client\V1\V1StorageClient;
+use GuzzleHttp\RequestOptions;
+
+class BergenAdapter implements StorageAdapterInterface
+{
+    /**
+     * @var \Bergen\Client\V1\V1StorageClient
+     */
+    private $client;
+
+    public static function build(array $config)
+    {
+        if (!array_key_exists('host', $config)) {
+            throw new InvalidArgumentException(
+                'Unable to build BergenAdapter: missing "host" from configuration.'
+            );
+        }
+        $config = [
+            ClientOptions::API_HOST => trim($config['host']),
+        ];
+        if (isset($config['username']) && isset($config['password'])) {
+            $config[RequestOptions::AUTH] = [
+                trim($config['username']),
+                $config['password'],
+            ];
+        }
+        if (array_key_exists('secure', $config)) {
+            $config[ClientOptions::SECURE_HTTP] = (bool) $config['secure'];
+        }
+
+        return new self(new V1StorageClient(new ClientBuilder($config)));
+    }
+
+    public function __construct(V1StorageClient $client)
+    {
+        $this->client = $client;
+    }
+
+    public function setData($key, $data)
+    {
+        try {
+            $this->client->put(rawurlencode($key), $data);
+        } catch (UnexpectedResponseError $e) {
+            throw new AdapterException('Unable to set data.', null, $e);
+        }
+    }
+
+    public function getData($key)
+    {
+        $response = null;
+        try {
+            $response = $this->client->get(rawurlencode($key));
+        } catch (UnexpectedResponseError $e) {
+            throw new AdapterException('Unable to get data.', null, $e);
+        }
+        return (string) $response->getBody();
+    }
+
+    public function deleteData($key)
+    {
+        try {
+            $this->client->delete(rawurlencode($key));
+        } catch (UnexpectedResponseError $e) {
+            throw new AdapterException('Unable to delete data.', null, $e);
+        }
+    }
+}
